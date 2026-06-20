2d10752791f3530fb4b327f3750e4379017b2890
justjess678 <jess.chambers@gmail.com>
2026-06-20 19:26:55 +0000

    Added Icewear to scraper list

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,9 +1,11 @@
 from .hobbii import HobbiiScraper
 from .ice_yarns import IceYarnsScraper
+from .icewear import IcewearScraper
 from .lovecrafts import LoveCraftsScraper
 
 SCRAPERS: dict[str, type] = {
     IceYarnsScraper.source_id: IceYarnsScraper,
     LoveCraftsScraper.source_id: LoveCraftsScraper,
     HobbiiScraper.source_id: HobbiiScraper,
+    IcewearScraper.source_id: IcewearScraper,
 }
