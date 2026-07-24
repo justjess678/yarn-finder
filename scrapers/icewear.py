a56d6da45a9123bc9a76ae0104703fdf6e428a33
justjess678 <jess.chambers@gmail.com>
2026-07-24 16:39:06 +0000

    Scale down chrome

--- scrapers/icewear.py
+++ scrapers/icewear.py
@@ -14,9 +14,6 @@ class IcewearScraper(BaseScraper):
 
     def scrape(self):
         driver = self._make_driver()
-        driver.execute_script(
-            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
-        )
         try:
             page_count = 1
             links = self._get_yarn_links(driver, page_count)
